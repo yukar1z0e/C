752ee7171419dbf857679360f93399a8cd936c66
chanzoe <yakumozoe@outlook.com>
2018-10-23 22:34:57 +0800

    C++

---
+++ Testshell/cmake-build-debug/CMakeFiles/Testshell.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Testshell.dir/main.cpp.obj"
+  "Testshell.pdb"
+  "Testshell.exe"
+  "Testshell.exe.manifest"
+  "libTestshell.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Testshell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Testshell/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void hello(){
+    printf("hello world");
+}
+
+int main() {
+    hello();
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
