41429a39a04d02f34afc28601a2ab20777d9371a
chanzoe <yakumozoe@outlook.com>
2018-10-24 12:14:56 +0800

    C++

---
+++ ZeroPipeBackdoor/cmake-build-debug/CMakeFiles/ZeroPipeBackdoor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZeroPipeBackdoor.dir/main.cpp.obj"
+  "ZeroPipeBackdoor.pdb"
+  "ZeroPipeBackdoor.exe"
+  "ZeroPipeBackdoor.exe.manifest"
+  "libZeroPipeBackdoor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZeroPipeBackdoor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
